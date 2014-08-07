c87224cffcd78a3efd1a649416226d379b79cfdb
martin <sgzs6721@126.com>
2014-08-07 15:45:28 +0800

    add sql

--- Vagetable.sql
+++ Vagetable.sql
@@ -1,23 +1,16 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.3
+-- version 3.1.3.1
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: 2014-08-06 23:34:37
--- 服务器版本： 5.6.19-enterprise-commercial-advanced
--- PHP Version: 5.4.24
+-- 主机: localhost
+-- 生成日期: 2014 年 08 月 07 日 15:44
+-- 服务器版本: 5.1.33
+-- PHP 版本: 5.2.9-2
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
--- Database: `Vagetable`
+-- 数据库: `vagetable`
 --
 
 -- --------------------------------------------------------
@@ -27,11 +20,17 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `category` (
-`id` int(4) NOT NULL,
+  `id` int(4) NOT NULL AUTO_INCREMENT,
   `name` varchar(8) COLLATE utf8_bin NOT NULL,
   `categoryid` tinyint(4) NOT NULL,
-  `superid` tinyint(4) NOT NULL
+  `superid` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+--
+-- 导出表中的数据 `category`
+--
+
 
 -- --------------------------------------------------------
 
@@ -40,20 +39,29 @@ CREATE TABLE IF NOT EXISTS `category` (
 --
 
 CREATE TABLE IF NOT EXISTS `member` (
-`id` int(8) NOT NULL,
-  `name` varchar(16) COLLATE utf8_bin NOT NULL,
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `username` varchar(16) COLLATE utf8_bin NOT NULL,
+  `realname` varchar(16) COLLATE utf8_bin DEFAULT NULL COMMENT '真实姓名',
+  `email` varchar(32) COLLATE utf8_bin DEFAULT NULL,
+  `phone` int(12) DEFAULT NULL,
   `passwd` varchar(64) COLLATE utf8_bin NOT NULL,
-  `permissioin` tinyint(4) NOT NULL,
+  `permission` tinyint(4) NOT NULL,
   `mdate` datetime NOT NULL,
-  `udate` int(11) DEFAULT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
+  `udate` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=8 ;
 
 --
--- 转存表中的数据 `member`
+-- 导出表中的数据 `member`
 --
 
-INSERT INTO `member` (`id`, `name`, `passwd`, `permissioin`, `mdate`, `udate`) VALUES
-(1, 'martin', '202cb962ac59075b964b07152d234b70', 0, '2014-08-06 09:00:00', NULL);
+INSERT INTO `member` (`id`, `username`, `realname`, `email`, `phone`, `passwd`, `permission`, `mdate`, `udate`) VALUES
+(1, 'martin', NULL, NULL, NULL, '202cb962ac59075b964b07152d234b70', 0, '2014-08-06 09:00:00', NULL),
+(2, '123', '123', '', 123, 'e10adc3949ba59abbe56e057f20f883e', 0, '2014-08-07 13:46:46', '0000-00-00 00:00:00'),
+(4, '1232', '123', '', 123, 'e10adc3949ba59abbe56e057f20f883e', 0, '2014-08-07 14:15:20', '2014-08-07 14:15:20'),
+(6, '12322', '123', '', 123, 'e10adc3949ba59abbe56e057f20f883e', 0, '2014-08-07 14:16:22', '2014-08-07 14:16:22'),
+(7, '123222', '123', '', 123, 'e10adc3949ba59abbe56e057f20f883e', 0, '2014-08-07 14:16:56', '2014-08-07 14:16:56');
 
 -- --------------------------------------------------------
 
@@ -62,13 +70,19 @@ INSERT INTO `member` (`id`, `name`, `passwd`, `permissioin`, `mdate`, `udate`) V
 --
 
 CREATE TABLE IF NOT EXISTS `order` (
-`id` int(8) NOT NULL,
+  `id` int(8) NOT NULL AUTO_INCREMENT,
   `userid` int(8) NOT NULL,
   `newdatetime` datetime NOT NULL,
   `updatedatetime` datetime NOT NULL,
   `addr` varchar(60) COLLATE utf8_bin NOT NULL,
-  `status` tinyint(4) NOT NULL
+  `status` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+--
+-- 导出表中的数据 `order`
+--
+
 
 -- --------------------------------------------------------
 
@@ -77,7 +91,7 @@ CREATE TABLE IF NOT EXISTS `order` (
 --
 
 CREATE TABLE IF NOT EXISTS `product` (
-`id` int(8) NOT NULL,
+  `id` int(8) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) COLLATE utf8_bin NOT NULL,
   `desc` varchar(80) COLLATE utf8_bin NOT NULL,
   `price` double NOT NULL,
@@ -85,8 +99,14 @@ CREATE TABLE IF NOT EXISTS `product` (
   `categoryid` tinyint(4) NOT NULL,
   `pdate` datetime NOT NULL,
   `picpath` varchar(50) COLLATE utf8_bin NOT NULL,
-  `unit` varchar(4) COLLATE utf8_bin NOT NULL
+  `unit` varchar(4) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+--
+-- 导出表中的数据 `product`
+--
+
 
 -- --------------------------------------------------------
 
@@ -95,12 +115,18 @@ CREATE TABLE IF NOT EXISTS `product` (
 --
 
 CREATE TABLE IF NOT EXISTS `saleitem` (
-`id` int(12) NOT NULL,
+  `id` int(12) NOT NULL AUTO_INCREMENT,
   `productid` tinyint(4) NOT NULL,
   `orderid` int(12) NOT NULL,
   `count` tinyint(4) NOT NULL,
-  `unitprice` double NOT NULL
+  `unitprice` double NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+--
+-- 导出表中的数据 `saleitem`
+--
+
 
 -- --------------------------------------------------------
 
@@ -109,7 +135,7 @@ CREATE TABLE IF NOT EXISTS `saleitem` (
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-`id` int(8) NOT NULL,
+  `id` int(8) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) COLLATE utf8_bin NOT NULL,
   `password` varchar(20) COLLATE utf8_bin NOT NULL,
   `phone` int(15) DEFAULT NULL,
@@ -117,91 +143,14 @@ CREATE TABLE IF NOT EXISTS `user` (
   `rdata` date NOT NULL,
   `grade` varchar(8) COLLATE utf8_bin NOT NULL,
   `score` int(8) NOT NULL,
-  `pocket` int(8) NOT NULL
+  `pocket` int(8) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
 
 --
--- 转存表中的数据 `user`
+-- 导出表中的数据 `user`
 --
 
 INSERT INTO `user` (`id`, `name`, `password`, `phone`, `address`, `rdata`, `grade`, `score`, `pocket`) VALUES
 (1, 'martin', '123', NULL, '', '0000-00-00', '0', 0, 1),
 (2, 'lei', '123', NULL, '', '0000-00-00', '0', 0, 0);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `category`
---
-ALTER TABLE `category`
- ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `member`
---
-ALTER TABLE `member`
- ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `order`
---
-ALTER TABLE `order`
- ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `product`
---
-ALTER TABLE `product`
- ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `saleitem`
---
-ALTER TABLE `saleitem`
- ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
- ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `category`
---
-ALTER TABLE `category`
-MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `member`
---
-ALTER TABLE `member`
-MODIFY `id` int(8) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `order`
---
-ALTER TABLE `order`
-MODIFY `id` int(8) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `product`
---
-ALTER TABLE `product`
-MODIFY `id` int(8) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `saleitem`
---
-ALTER TABLE `saleitem`
-MODIFY `id` int(12) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-MODIFY `id` int(8) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
