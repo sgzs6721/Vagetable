ca3022cc57845369c6fe2d843b33259af231ee68
martin <sgzs6721@126.com>
2014-08-19 16:41:58 +0800

    modify home page for image upload

--- application/views/home.php
+++ application/views/home.php
@@ -2,7 +2,7 @@
 
 <a href="<?php echo site_url('member/add');?>">添加用户</a><br><br>
 <a href="<?php echo site_url('member/list_members');?>">查看所有用户</a><br><br><br>
-<a href="<?php echo site_url('product/add/');?>">商品录入</a><br><br>
+<a href="<?php echo site_url('product/add');?>">商品录入</a><br><br>
 <a href="<?php echo site_url('product/list_products');?>">查看所有商品</a><br><br><br>
-<a href="<?php echo site_url('image');?>">图片上传</a><br><br><br>
+<a href="<?php echo site_url('image/select_category');?>">图片上传</a><br><br><br>
 <a href="<?php echo site_url('member/logout');?>">登出</a><br><br>
