06a97fbea372157a6d4192ce052411ce8d793c1d
martin <sgzs6721@126.com>
2014-08-19 16:33:04 +0800

    modify database for the upload image

--- Vagetable.sql
+++ Vagetable.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2014 年 08 月 18 日 04:54
+-- 生成日期: 2014 年 08 月 19 日 08:31
 -- 服务器版本: 5.1.33
 -- PHP 版本: 5.2.9-2
 
@@ -28,19 +28,21 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 CREATE TABLE IF NOT EXISTS `category` (
   `id` int(4) NOT NULL AUTO_INCREMENT,
   `name` varchar(8) COLLATE utf8_bin NOT NULL,
+  `enname` varchar(16) COLLATE utf8_bin DEFAULT NULL,
   `categoryid` tinyint(4) NOT NULL,
   `superid` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=5 ;
 
 --
 -- 导出表中的数据 `category`
 --
 
-INSERT INTO `category` (`id`, `name`, `categoryid`, `superid`) VALUES
-(1, '蔬菜', 1, 0),
-(2, '水果', 2, 0),
-(3, '生鲜', 3, 0);
+INSERT INTO `category` (`id`, `name`, `enname`, `categoryid`, `superid`) VALUES
+(1, '蔬菜', 'vegetable', 1, 0),
+(2, '水果', 'fruit', 2, 0),
+(3, '生鲜', 'fresh', 3, 0),
+(4, '其它', 'other', 4, 0);
 
 -- --------------------------------------------------------
 
@@ -110,11 +112,11 @@ CREATE TABLE IF NOT EXISTS `product` (
   `category` tinyint(4) NOT NULL,
   `pdate` datetime NOT NULL,
   `udate` datetime NOT NULL,
-  `picpath` varchar(50) COLLATE utf8_bin NOT NULL,
+  `picpath` varchar(200) COLLATE utf8_bin NOT NULL,
   `unit` varchar(4) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=21 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=33 ;
 
 --
 -- 导出表中的数据 `product`
@@ -126,7 +128,17 @@ INSERT INTO `product` (`id`, `name`, `desc`, `oprice`, `sprice`, `mprice`, `cate
 (14, '大区d', '', 123, 123, 123, 1, '2014-08-18 03:25:58', '2014-08-18 03:25:58', '', ''),
 (16, '大区djkdf', '', 123, 123, 123, 1, '2014-08-18 03:27:04', '2014-08-18 03:27:04', '', ''),
 (17, 'sgzs', '', 12, 123, 12, 1, '2014-08-18 04:05:13', '2014-08-18 04:05:13', '', ''),
-(19, 'sgzs23dd', '', 12, 123, 12, 1, '2014-08-18 04:06:32', '2014-08-18 04:19:16', '', '');
+(19, 'sgzs23dd', '', 12, 123, 12, 1, '2014-08-18 04:06:32', '2014-08-18 04:19:16', '', ''),
+(21, 'sdf', '12', 12, 12, 12, 1, '2014-08-18 07:55:22', '2014-08-18 07:55:22', '', ''),
+(22, 'sdfd', '12', 12, 12, 12, 1, '2014-08-18 07:56:31', '2014-08-18 07:56:31', '', ''),
+(23, 'sdfdd', '12', 12, 12, 12, 1, '2014-08-18 07:57:35', '2014-08-18 07:57:35', '', ''),
+(24, 'sdfdd1', '12', 12, 12, 12, 1, '2014-08-18 08:04:06', '2014-08-18 08:04:06', '', ''),
+(25, 'dfdfadfad', '123', 123, 123, 123, 1, '2014-08-18 08:04:37', '2014-08-18 08:04:37', '', ''),
+(26, '大起大落', '123', 123, 123, 123, 1, '2014-08-18 08:32:17', '2014-08-18 08:32:17', '', ''),
+(27, '大起大落d', '123', 123, 123, 123, 1, '2014-08-18 08:36:16', '2014-08-18 08:36:16', '/Vagetable/images/thumbnail/1.jpg;/Vagetable/images/thumbnail/2.jpg;/Vagetable/images/thumbnail/3.jpg', ''),
+(28, '大起大落dd', '123', 123, 123, 123, 1, '2014-08-18 08:46:43', '2014-08-18 08:46:43', '/Vagetable/images/thumbnail/1.jpg;/Vagetable/images/thumbnail/2.jpg', ''),
+(30, '大起大落dd3', '123', 123, 123, 123, 1, '2014-08-18 08:47:06', '2014-08-18 08:47:06', '/Vagetable/images/thumbnail/1.jpg;/Vagetable/images/thumbnail/2.jpg;/Vagetable/images/thumbnail/3.jpg', ''),
+(32, '大起大落dd32', '123', 123, 123, 123, 1, '2014-08-18 08:47:34', '2014-08-18 08:47:34', '/Vagetable/images/thumbnail/1.jpg;/Vagetable/images/thumbnail/2.jpg;/Vagetable/images/thumbnail/3.jpg', '');
 
 -- --------------------------------------------------------
 
